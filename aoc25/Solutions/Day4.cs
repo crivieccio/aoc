66ccd1b2f71854d54507ca599ff3dcf1dd992634
Chris Rivieccio <criviecc@gmail.com>
2025-12-03 21:26:10 -0500

    chore: set up day4

---
+++ aoc25/Solutions/Day4.cs
@@ -0,0 +1,9 @@
+
+namespace Solutions;
+
+public class Day4 : BaseDay
+{
+  public override string Part1(List<string> input) => throw new NotImplementedException();
+
+  public override string Part2(List<string> input) => throw new NotImplementedException();
+}
---
+++ aoc25/Tests/Day4Test.cs
@@ -0,0 +1,46 @@
+using Solutions;
+using Xunit.Abstractions;
+
+namespace Tests;
+
+public class Day4Test(ITestOutputHelper output) : BaseDayTest<Day4>(output)
+{
+  protected override int DayNumber => 4;
+
+  protected override string Part1ExampleExpected => throw new NotImplementedException();
+
+  protected override string Part2ExampleExpected => throw new NotImplementedException();
+
+  protected override bool TestAgainstActualInput => false;
+
+  protected override void ProcessInput(Day4 instance, List<string> input)
+  {
+    // This test framework calls ProcessInput for both Part1 and Part2 tests
+    // We need to determine which part to run based on the context
+
+    // Check if we're being called from Part1 or Part2 test by examining the stack
+    var stackTrace = new System.Diagnostics.StackTrace();
+    var testName = stackTrace.GetFrames()
+        .Select(f => f.GetMethod()?.Name)
+        .FirstOrDefault(m => m?.Contains("Part1_ExampleInput_ShouldMatchExpected") == true ||
+                            m?.Contains("Part2_ExampleInput_ShouldMatchExpected") == true);
+
+    if (testName?.Contains("Part1_") == true)
+    {
+      var result = instance.Part1(input);
+      Console.WriteLine(result);
+    }
+    else if (testName?.Contains("Part2_") == true)
+    {
+      var result = instance.Part2(input);
+      Console.WriteLine(result);
+    }
+    else
+    {
+      var result1 = instance.Part1(input);
+      var result2 = instance.Part2(input);
+      Console.WriteLine($"Part1: {result1}");
+      Console.WriteLine($"Part2: {result2}");
+    }
+  }
+}
