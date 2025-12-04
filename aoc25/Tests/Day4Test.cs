b37b25c4dd6d7e5f5bcbe88d56e0c33dd8e0fe28
Chris Rivieccio <criviecc@gmail.com>
2025-12-04 09:28:12 -0500

    day 4 complete

--- aoc25/Tests/Day4Test.cs
+++ aoc25/Tests/Day4Test.cs
@@ -7,9 +7,9 @@ public class Day4Test(ITestOutputHelper output) : BaseDayTest<Day4>(output)
 {
   protected override int DayNumber => 4;
 
-  protected override string Part1ExampleExpected => throw new NotImplementedException();
+  protected override string Part1ExampleExpected => "13";
 
-  protected override string Part2ExampleExpected => throw new NotImplementedException();
+  protected override string Part2ExampleExpected => "43";
 
   protected override bool TestAgainstActualInput => false;
 
