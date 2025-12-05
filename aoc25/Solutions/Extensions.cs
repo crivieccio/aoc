7a57cb1085c291db652d4015ac85eb92c4258317
Chris Rivieccio <criviecc@gmail.com>
2025-12-05 09:36:31 -0500

    refactor: remove range extensions

--- aoc25/Solutions/Extensions.cs
+++ aoc25/Solutions/Extensions.cs
@@ -10,12 +10,4 @@ public static class Extensions
       yield return i;
     }
   }
-
-  public static Range Merge(this Range range, Range other) => new(Math.Min(range.Low, other.Low), Math.Max(range.High, other.High));
-
-  public static bool HasOverlap(this Range range, Range other) => LowInRange(range, other) || HighInRange(range, other);
-
-  public static bool LowInRange(this Range range, Range other) => range.Low >= other.Low;
-
-  public static bool HighInRange(this Range range, Range other) => range.High <= other.High;
 }
