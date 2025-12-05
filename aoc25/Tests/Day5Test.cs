d5ad758a4259b25a94b5afbfe2670f2a10b8dacf
Chris Rivieccio <criviecc@gmail.com>
2025-12-05 09:36:42 -0500

    day 5 finished

--- aoc25/Tests/Day5Test.cs
+++ aoc25/Tests/Day5Test.cs
@@ -7,9 +7,9 @@ public class Day5Test(ITestOutputHelper output) : BaseDayTest<Day5>(output)
 {
   protected override int DayNumber => 5;
 
-  protected override string Part1ExampleExpected => "-1";
+  protected override string Part1ExampleExpected => "3";
 
-  protected override string Part2ExampleExpected => "-1";
+  protected override string Part2ExampleExpected => "14";
 
   protected override bool TestAgainstActualInput => false;
 }
