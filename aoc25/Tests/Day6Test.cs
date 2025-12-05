5a6468347a340337a5f52373ee8eeaada5d41283
Chris Rivieccio <criviecc@gmail.com>
2025-12-05 09:43:43 -0500

    chore: setup day 6 solution and test

---
+++ aoc25/Tests/Day6Test.cs
@@ -0,0 +1,15 @@
+using Solutions;
+using Xunit.Abstractions;
+
+namespace Tests;
+
+public class Day6Test(ITestOutputHelper output) : BaseDayTest<Day6>(output)
+{
+  protected override int DayNumber => 6;
+
+  protected override string Part1ExampleExpected => "3";
+
+  protected override string Part2ExampleExpected => "14";
+
+  protected override bool TestAgainstActualInput => false;
+}
