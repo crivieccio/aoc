6453d953b93d4e58f077ea5d097e583ef96b62b5
Chris Rivieccio <criviecc@gmail.com>
2025-12-07 12:41:13 -0500

    chore: setup day 8

---
+++ aoc25/Tests/Day8Test.cs
@@ -0,0 +1,14 @@
+using Solutions;
+
+using Tests;
+
+using Xunit.Abstractions;
+
+public class Day8Test(ITestOutputHelper output) : BaseDayTest<Day8>(output)
+{
+    protected override int DayNumber => 8;
+
+    protected override string Part1ExampleExpected => "0";
+
+    protected override string Part2ExampleExpected => "0";
+}
