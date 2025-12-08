7e5872cf572e6763d48b30c6320d27fe27e8fc2d
Chris Rivieccio <criviecc@gmail.com>
2025-12-08 12:58:27 -0500

    chore: set up day9 files

---
+++ aoc25/Tests/Day9Test.cs
@@ -0,0 +1,14 @@
+using Solutions;
+
+using Xunit.Abstractions;
+
+namespace Tests;
+
+public class Day9Test(ITestOutputHelper output) : BaseDayTest<Day9>(output)
+{
+    protected override int DayNumber => 9;
+
+    protected override string Part1ExampleExpected => throw new NotImplementedException();
+
+    protected override string Part2ExampleExpected => throw new NotImplementedException();
+}
