41cd7a0ca4449b59996187fedf0e07c51c485865
Chris Rivieccio <criviecc@gmail.com>
2025-12-09 07:10:11 -0500

    day 9 part 1 complete

--- aoc25/Solutions/Day9.cs
+++ aoc25/Solutions/Day9.cs
@@ -1,8 +1,36 @@
 
 namespace Solutions;
 
+public record struct Point(long X, long Y);
 public class Day9 : BaseDay
 {
-    public override string Part1(List<string> input) => throw new NotImplementedException();
+    public override string Part1(List<string> input) =>
+        input
+        .ParsePoints()
+        .GetAllPairs()
+        .Select(p => CalculateArea(p.Item1, p.Item2))
+        .Max()
+        .ToString();
+
     public override string Part2(List<string> input) => throw new NotImplementedException();
+
+    private static long CalculateArea(Point a, Point b)
+    {
+        return a.X == b.X ? Math.Abs(a.X - b.X) + 1
+            : a.Y == b.Y ? Math.Abs(a.Y - b.Y) + 1
+            : (Math.Abs(a.Y - b.Y) + 1) * (Math.Abs(a.X - b.X) + 1);
+    }
+}
+
+public static class Day9Extensions
+{
+    extension(List<string> source)
+    {
+        public IEnumerable<Point> ParsePoints() => source.Select(s => s.Split(',')).Select(s => new Point(long.Parse(s[0]), long.Parse(s[1])));
+    }
+
+    extension(IEnumerable<Point> source)
+    {
+        public IEnumerable<(Point, Point)> GetAllPairs() => source.SelectMany((_, i) => source.Where((_, j) => i < j), (a, b) => (a, b));
+    }
 }
--- aoc25/Tests/Day9Test.cs
+++ aoc25/Tests/Day9Test.cs
@@ -8,7 +8,7 @@ public class Day9Test(ITestOutputHelper output) : BaseDayTest<Day9>(output)
 {
     protected override int DayNumber => 9;
 
-    protected override string Part1ExampleExpected => throw new NotImplementedException();
+    protected override string Part1ExampleExpected => "50";
 
-    protected override string Part2ExampleExpected => throw new NotImplementedException();
+    protected override string Part2ExampleExpected => "0";
 }
