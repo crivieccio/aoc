99e3d2e461f5eed418e54504964df9dca20a07d1
Chris Rivieccio <criviecc@gmail.com>
2025-12-09 12:34:38 -0500

    chore: setup day 10

---
+++ aoc25/Tests/Day10Test.cs
@@ -0,0 +1,14 @@
+using Solutions;
+
+using Xunit.Abstractions;
+
+namespace Tests;
+
+public class Day10Test(ITestOutputHelper output) : BaseDayTest<Day10>(output)
+{
+    protected override int DayNumber => 10;
+
+    protected override string Part1ExampleExpected => "0";
+
+    protected override string Part2ExampleExpected => "0";
+}
