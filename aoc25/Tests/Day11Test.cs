1b2411fd74064a0aebc8cb14bdc9370281caaf39
Chris Rivieccio <criviecc@gmail.com>
2025-12-10 12:50:34 -0500

    chore: setup day 11 files

---
+++ aoc25/Tests/Day11Test.cs
@@ -0,0 +1,13 @@
+using Solutions;
+
+using Xunit.Abstractions;
+
+namespace Tests;
+
+
+public class Day11Test(ITestOutputHelper output) : BaseDayTest<Day11>(output)
+{
+    protected override int DayNumber => 11;
+    protected override string Part1ExampleExpected => "0";
+    protected override string Part2ExampleExpected => "0";
+}
